c59127d22e8a5c09bb9e534356be379182c6956d
smouktar <sulymanmouktar@yahoo.com>
2024-01-15 15:31:23 +0100

    added

---
+++ python-test_driven_development/tests/6-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import path
+
+path.append('../')
+max_integer = __import__('6-max_integer').max_integer
+
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
