808f6af3c5e63e93f73d4594286552f7155714e3
smouktar <sulymanmouktar@yahoo.com>
2024-01-15 15:33:25 +0100

    add file

--- python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    matrix_square = [[num**2 for num in row] for row in matrix]
-    return matrix_square
--- python-more_data_structures/2-uniq_add.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-    result = sum(num for num in set(my_list))
-    return result
